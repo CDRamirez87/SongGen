c1647bf82f0c3c4375b3499f3a7604a9f208d79f
KM Ramirez <c.d.ramirez1987@gmail.com>
2020-02-25 10:01:48 -0800

    Added user input, program will extrapolate it into a scale, cleaned up structure

--- main.cpp
+++ main.cpp
@@ -3,23 +3,11 @@
 #include <cstdlib> // for std::rand() and std::srand()
 #include <ctime> // for std::time()
 
-enum Note
-{
-	NOTE_A,		//0
-	NOTE_ASHARP,	//1
-	NOTE_B,		//2
-	NOTE_C,		//3
-	NOTE_CSHARP,	//4
-	NOTE_D,		//5
-	NOTE_DSHARP,	//6
-	NOTE_E,		//7
-	NOTE_F,		//8
-	NOTE_FSHARP,	//9
-	NOTE_G,		//10
-	NOTE_GSHARP	//11
-};
-
-int prog[][5] //List of chord progressions
+//CONSTANTS
+const int PROG_INDEX = 16;
+const int MAJOR_KEY[] = {0, 2, 4, 5, 7, 9, 11},
+		  MINOR_KEY[] = {0, 2, 3, 5, 7, 8, 10};
+const int PROG[PROG_INDEX][5] //List of chord progressions
 {
 	{1, 5, 6, 4, 0},
 	{1, 5, 6, 3, 4},
@@ -40,6 +28,25 @@ int prog[][5] //List of chord progressions
 	
 };
 
+//SPECIAL DATA TYPES
+enum Note
+{
+	NOTE_A,		//0
+	NOTE_ASHARP,	//1
+	NOTE_B,		//2
+	NOTE_C,		//3
+	NOTE_CSHARP,	//4
+	NOTE_D,		//5
+	NOTE_DSHARP,	//6
+	NOTE_E,		//7
+	NOTE_F,		//8
+	NOTE_FSHARP,	//9
+	NOTE_G,		//10
+	NOTE_GSHARP	//11
+};
+
+/*-----------------------FUNCTIONS-------------------------*/
+
 // Generate a random number between 1 and max (inclusive)
 // Assumes std::srand() has already been called
 // Assumes max - min <= RAND_MAX
@@ -50,7 +57,8 @@ int rollDie(int sides)
     return 1 + static_cast<int>((sides - 2) * (std::rand() * fraction));
 }
 
-void setMajorKey(Note note, Note key[], const int interval[]) //For sharp keys
+//Extrapolates the tonic into a scale
+void setMajorKey(Note note, Note key[], const int interval[]) 
 {
 		for(int degree = 0; degree < 7; degree++)
 		{
@@ -61,19 +69,93 @@ void setMajorKey(Note note, Note key[], const int interval[]) //For sharp keys
 		}
 }
 
+//Lets the user choose a key for the song
+int getUserKey() 
+{
+	int userChoice(0);
+	
+	std::cout << "1. C \n 2. C# \n 3. D \n 4. Eb \n 5. E \n 6. F \n 7. Gb \n 8. G \n 9. Ab \n 10. A \n 11. Bb \n 12. B \n";
+					
+	
+	while (userChoice < 1 || userChoice > 12)	
+	{
+		userChoice = 0;
+		std::cout << "Enter a number (1-12) and press Enter to Select a Key:";
+		std::cin >> userChoice;
+	}
+	
+	return userChoice;
+}
+
+//Determines whether the key should be displayed with sharps or flats
+bool isKeySharp(int key)
+{
+	if (key == 1 || key == 2 || key == 3 || key == 5 || key == 8 || key == 10 || key == 12)
+			return true;
+	
+	else
+		return false;
+}
+
+//Equates the user's input to a key
+Note setTonic (int key)
+{
+	switch (key)
+	{
+		case 1:
+			return NOTE_C;
+		case 2:
+			return NOTE_CSHARP;
+		case 3:
+			return NOTE_D;
+		case 4:
+			return NOTE_DSHARP;
+		case 5:
+			return NOTE_E;
+		case 6:
+			return NOTE_F;
+		case 7:
+			return NOTE_FSHARP;
+		case 8:
+			return NOTE_G;
+		case 9:
+			return NOTE_GSHARP;
+		case 10:
+			return NOTE_A;
+		case 11: 
+			return NOTE_ASHARP;
+		case 12: 
+			return NOTE_B;
+		default:
+			std::cout << "ERROR IN Note setKey";
+			break;
+	}
+			
+}
+
+
+/*-------------------------------------------MAIN------------------------------------------------*/
+
 
 int main()
 {	
-	const int MAJOR_KEY[] = {0, 2, 4, 5, 7, 9, 11},
-			  MINOR_KEY[] = {0, 2, 3, 5, 7, 8, 10};
-	
 	std::srand(static_cast<unsigned int>(std::time(nullptr))); // set initial seed value for random numbers to system clock
 	
+	int userKey = getUserKey(); //Prompts user for input and returns it as an integer
+	
+	Note songChords[7], keyTonic;
+	
+	keyTonic = setTonic(userKey);	//gets the tonic as a Note to extrapolate into a scale
+	
+	setMajorKey(keyTonic, songChords, MAJOR_KEY); //Extrapolates the tonic into a scale, assigns to songChords by reference
+	
+	bool isSharp = isKeySharp(userKey);
+	
+	//TODO - randomly select chord progressions and traspose them to key, output in sections
+	
 	
 		
-		
-		
-	//*******************************TEST*********************************//
+	/*******************************TEST*********************************
 	
 	Note dMajor[7];
 	
@@ -82,7 +164,10 @@ int main()
 	for (int i = 0; i < 6; i++)
 		std::cout << dMajor[i] << "\n";
 	
+	*/
 	
+	for (int i = 0; i < 100 ; i++)
+		std::cout << rollDie(6) << "\n";
 	
 	return 0;
 
